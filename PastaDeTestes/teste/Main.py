50bafdb3641def6e667c938e77a21eb1819198da
JoaquimGon <joaquimmcgoncalves@gmail.com>
2023-10-04 12:22:08 +0100

    windows

--- PastaDeTestes/teste/Main.py
+++ PastaDeTestes/teste/Main.py
@@ -1,16 +1,4 @@
-import tkinter as tk
-from add_widgets import add_widgets
-from replace_widgets import replace_widgets
-
-# Create the main window
-window = tk.Tk()
-
-# Create a frame
-frame = tk.Frame(window)
-frame.pack()
-
-# Create a button to trigger widget replacement
-add_widgets(frame)
-
-# Start the Tkinter event loop
-window.mainloop()
+test=1
+test2 = 5
+if (1<=test>0) and (10>=test2>=5):
+    print(True)
--- windows.py
+++ windows.py
@@ -1,14 +1,27 @@
 import tkinter as tk
 from tkinter import ttk
 from Windows.loginToW import ToMain
-from UsersInfo.Credentials import EnterCredentials
+from UsersInfo.Credentials import EnterCredentials, SaveCredentials
 from Misc.Warning import AddWarning
 
 
-def ClearFrame(frame):
-    for widget in frame.winfo_children():
+
+def ClearWindow(Frame):
+    for widget in Frame.winfo_children():
         widget.destroy()
-    return frame
+
+def ChangWindo(Frame, Window, Save, Enter):
+    Credentials = {"UserName": UserNameInput.get(), "Email": EmailInput.get(), "Password": PasswordInput.get()}
+
+    if Window == "LoginMain":
+        check = EnterCredentials(Credentials)
+        if check:
+            ClearWindow(Frame)
+            MainSection(Frame)
+        elif isinstance(check, str):
+            AddWarning(check, WarningLabel, ChangeToRegister, ChangeToLogin)
+
+
 
 def LoginSection(Frame):
     TitleLabel = ttk.Label(Frame, text="Login Window", font=25)  # Title
@@ -37,6 +50,7 @@ def LoginSection(Frame):
     global WarningLabel
     WarningLabel = ttk.Label(Frame, text="", font=12)
 
+
 def RegisterSection(Frame):
     TitleLabel = ttk.Label(Frame, text="Register Window", font=25)
     TitleLabel.grid(row=0, columnspan=2)
@@ -63,9 +77,9 @@ def RegisterSection(Frame):
     Register = ttk.Button(Frame, text="Register", width=20, command=PreparingToMain(Frame))
     Register.grid(row=4, columnspan=2, sticky=tk.W, padx=2, pady=2)
 
-    global ChangeToLogIn
-    ChangeToLogIn = ttk.Button(Frame, text="LogIn", width=20, command=lambda: Warning("TEST"))
-    ChangeToLogIn.grid(row=4, column=1, sticky=tk.E, pady=2)
+    global ChangeToLogin
+    ChangeToLogin = ttk.Button(Frame, text="LogIn", width=20, command=lambda: Warning("TEST"))
+    ChangeToLogin.grid(row=4, column=1, sticky=tk.E, pady=2)
 
     global WarningLabel
     WarningLabel = ttk.Label(Frame, text="", font=12)
